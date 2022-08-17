a34801a07492c9db8542b28fab6790d1cfc95061
sharath0033 <sharathchandra0033@gmail.com>
2022-08-18 01:57:41 +0530

    No Content related logic added

--- src/dashboards/dashboards.service.ts
+++ src/dashboards/dashboards.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { FirebaseFirestoreService } from '@aginix/nestjs-firebase-admin';
 
 import { DashboardDto } from './dto/dasboard.dto';
 import { Dashboard } from './entities/dasboard';
 
 @Injectable()
 export class DashboardsService {
-    constructor() { }
+    constructor(
+        private fbFirestore: FirebaseFirestoreService,
+    ) { }
 
     async listDashboards(): Promise<Dashboard[]> {
-        return;
+        let dasboardsList: Dashboard[] = [];
+        const querySnapshots = await this.fbFirestore.collection('dashboards').get();
+        querySnapshots.forEach(snapshot => {
+            const dashboard: any = snapshot.data();
+            dasboardsList.push({
+                id: snapshot.id,
+                name: dashboard.name
+            });
+        })
+        return dasboardsList;
     }
 
 
     async getDashboard(dashboardId: string): Promise<Dashboard> {
-        return;
+        const querySnapshot = await this.fbFirestore.collection('dashboards').doc(dashboardId).get();
+        const dashboard: any = querySnapshot.data();
+        return {
+            id: querySnapshot.id,
+            ...dashboard,
+        }
     }
 
 
