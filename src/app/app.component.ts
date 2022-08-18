131189f1c1e4bd31a5d479fd2f7c03f5d642e65d
sharath0033 <sharathchandra0033@gmail.com>
2022-08-18 17:12:54 +0530

    Preloaders added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
+import { ApplicationService } from './core/application';
 import { AppService } from './services';
 
 @Component({
@@ -11,9 +12,18 @@ import { AppService } from './services';
 export class AppComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription = new Subscription();
   isAppPreloader: boolean = false;
+  isPreloader: boolean = false;
   dashboardsList: any[] = [];
 
-  constructor(private appService: AppService) { }
+  constructor(
+    private applicationService: ApplicationService,
+    private appService: AppService
+  ) {
+    this.subscriptions.add(
+      this.applicationService.getPreloaderState()
+        .subscribe((_value: boolean) => this.isAppPreloader = _value)
+    )
+  }
 
   ngOnInit() {
     this.subscriptions.add(
@@ -23,12 +33,12 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   initializeData(): void {
-    this.isAppPreloader = true;
+    this.isPreloader = true;
     this.subscriptions.add(
       this.appService.getDashboardsList()
         .subscribe({
           next: (response) => this.dashboardsList = response,
-          complete: () => this.isAppPreloader = false
+          complete: () => this.isPreloader = false
         })
     );
   }
